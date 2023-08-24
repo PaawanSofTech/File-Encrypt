3b85e8e118212fe8e0613b95852b6eff9687dad4
Ankush Sinha <97420654+ankushSinha1@users.noreply.github.com>
2023-08-24 11:40:12 +0530

    Homepage navbar made

--- client/src/Components/Home/Homepage.js
+++ client/src/Components/Home/Homepage.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const Homepage = () => {
+  //Use effect to get the logged in details, to accordingly load user and company homepage
   return (
     <div>
-      
+      <div className='navbar' style={{display: 'flex', justifyContent: 'space-between', alignItems:'center'}}>
+        <div className='navbar__left'></div>
+        <div className='navbar__center'>
+          <h1 className='navbar__center--brand'>Group Project</h1>
+        </div>
+        <div className='navbar__right'>
+          <button className='navbar__right--edit'>
+            Edit details
+          </button>
+          <button className='navbar__right--connect'>
+            Connect
+          </button>
+        </div>
+      </div>
+
+
     </div>
   )
 }
