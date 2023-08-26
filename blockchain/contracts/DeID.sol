a61168c8b5ee1e0fee323508d2dd647e98abae49
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2023-08-26 12:05:25 +0530

    Completed Hardhat Setup

---
+++ blockchain/contracts/DeID.sol
@@ -0,0 +1,204 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract DeID{
+
+    address public owner;
+ 
+    constructor(){
+        owner = msg.sender;
+    }
+
+    struct Documents{
+        string Hash;
+        uint TimeAdded;
+    }
+    
+    struct User{
+        uint128 DOB;
+        uint64 _foldCount;
+        uint64 Phone;
+        address Address;
+        string Name;
+        string Image;
+        string[] Folders;
+    }
+
+    struct Employee{
+        string Name;
+        string Designation;
+        string Image;
+        string Id;
+    }
+
+    struct Company{
+        uint64 Phone;
+        uint64 RegistrationId;
+        string Name;
+        string Image;
+        string[] Employee;
+    }
+
+    struct Request{
+        address Address;
+        bool DOB;
+        bool Phone;
+        bool Name;
+        bool Image;
+        bool[] Folders;
+    }
+
+    // -- User --
+
+    // mapping( address => address[]) private Nominee;
+    //add nomminees
+
+    mapping( bytes32 => User) private userList;
+    mapping( bytes32 => mapping( uint => Documents[])) private userDocs;
+    mapping( address => bool) private userRegistered;
+
+
+    mapping( bytes32 => Company) private companyList;
+    mapping( bytes32 => mapping( address => Documents[])) private companyDocs;
+    mapping( address => bool) private companyRegistered;
+
+    // -- Company --
+
+    mapping( address => Request[]) private userRequests;
+
+    mapping( bytes32 => User[]) private companyData;
+
+    mapping( address => Request[]) private activeCompanies;
+
+    mapping( address => mapping(address => bool)) private companyAccess;
+
+    //add documents for company also!
+
+    function createUser(uint128 DOB,uint64 Phone,string calldata Name,string calldata Image) public{
+        require(userRegistered[msg.sender] == false,"User Already Registered");
+        require(companyRegistered[msg.sender] == false,"Registered as company");
+        bytes32 _val = keccak256(abi.encodePacked(msg.sender));
+        userList[_val].DOB = DOB;
+        userList[_val]._foldCount = 3; 
+        userList[_val].Folders.push("Medical");
+        userList[_val].Folders.push("Academics");
+        userList[_val].Folders.push("Bank");
+        userList[_val].Phone = Phone;
+        userList[_val].Name = Name;
+        userList[_val].Address= msg.sender;
+        userList[_val].Image = Image;
+        userRegistered[msg.sender] = true;
+    }
+
+    function userDetails() public view returns(User memory){
+        require(userRegistered[msg.sender],"Not Registered");
+        return userList[keccak256(abi.encodePacked(msg.sender))];
+    }
+
+    function addDocs(uint _id ,string calldata _hash) public{
+        require(userRegistered[msg.sender] || companyRegistered[msg.sender],"Not Registered");
+        Documents memory document = Documents(_hash,block.timestamp);
+        userDocs[keccak256(abi.encodePacked(msg.sender))][_id].push(document);
+    }
+
+    function returnDocs(uint _id)public view returns(Documents[] memory){
+        return userDocs[keccak256(abi.encodePacked(msg.sender))][_id];
+    }
+
+    function createCompany(uint64 Phone,uint64 RegistrationId,string calldata Name,string calldata Image) public{
+        require(userRegistered[msg.sender] == false,"Company Already Registered");
+        require(companyRegistered[msg.sender] == false,"Registered as User");
+        bytes32 _val = keccak256(abi.encodePacked(msg.sender));
+        companyList[_val].Phone = Phone;
+        companyList[_val].RegistrationId = RegistrationId;
+        companyList[_val].Name = Name;
+        companyList[_val].Image = Image; 
+        companyRegistered[msg.sender] = true;
+    }
+
+    function companyDetails(address _address) public view returns(Company memory){
+        return companyList[keccak256(abi.encodePacked(_address))];
+    }
+
+    function requestDetails(address _address,bool DOB,bool Phone,bool Name,bool Image,bool[] calldata _Folders) public {
+        require(companyRegistered[msg.sender] ,"Only Companies Can Request");
+        require(userRegistered[_address] ,"User Not registered");
+        Request memory request;
+        request.Address = msg.sender;
+        request.DOB = DOB;
+        request.Phone = Phone;
+        request.Name = Name;
+        request.Image = Image;
+        request.Folders = _Folders;
+        // Request(msg.sender,DOB,Phone,Name,Name,_Folders);
+        userRequests[_address].push(request);
+    }
+
+    function acceptDetails( uint _id) public{
+        Request memory request = userRequests[msg.sender][_id];
+        User memory _user = userList[ keccak256(abi.encodePacked(msg.sender))];
+        User memory newUser;
+        newUser._foldCount =_user._foldCount;
+        newUser.Address =_user.Address;
+        newUser.Folders = _user.Folders;
+        if(request.DOB ){
+            newUser.DOB =_user.DOB;
+        } 
+        if(request.Phone){
+            newUser.Phone = _user.Phone;
+        }
+        if(request.Name){
+            newUser.Name = _user.Name;
+        }
+        if(request.Image){
+            newUser.Image = _user.Image;
+        }
+        for(uint i =0 ; i< _user._foldCount-1 ; i++){
+            if(request.Folders[i]){
+                companyDocs[keccak256(abi.encodePacked(request.Address))][msg.sender] = userDocs[keccak256(abi.encodePacked(msg.sender))][i];
+                break;
+            }
+        }
+        companyData[keccak256(abi.encodePacked(request.Address))].push(newUser); 
+        activeCompanies[msg.sender].push(request);
+        companyAccess[request.Address][msg.sender] = true;
+        userRequests[msg.sender][_id] = userRequests[msg.sender][userRequests[msg.sender].length-1];
+        userRequests[msg.sender].pop();
+    }
+
+    function rejectDetails( uint _id) public{
+        userRequests[msg.sender][_id] = userRequests[msg.sender][userRequests[msg.sender].length-1];
+        userRequests[msg.sender].pop(); 
+    }
+
+    function returnUsersData() public view returns(User[] memory){
+        require(companyRegistered[msg.sender],"Not a Registered Company");
+        return companyData[keccak256(abi.encodePacked(msg.sender))];
+    }
+
+    function returnUserRequests() public view returns(Request[] memory){
+        require(userRegistered[msg.sender],"Register as User");
+        return userRequests[msg.sender];
+    }
+
+    function returnUserDocs(address Address) public view returns(Documents[] memory){
+        require(companyRegistered[msg.sender],"Not a Registered Company");
+        require(companyAccess[msg.sender][Address],"You Don't Have Access to User");
+        return companyDocs[keccak256(abi.encodePacked(msg.sender))][Address];
+    }
+
+    function revokeAccess(address Address) public {
+        require(companyAccess[Address][msg.sender],"Access Already Revoked");
+        companyAccess[Address][msg.sender] = false;
+        for(uint i =0; i< activeCompanies[msg.sender].length -1 ;i++){
+            if(activeCompanies[msg.sender][i].Address == Address){
+                activeCompanies[msg.sender][i] = activeCompanies[msg.sender][activeCompanies[msg.sender].length -1];
+                activeCompanies[msg.sender].pop();
+            }
+        }
+    }
+
+    //add events
+
+    //add employees
+}
--- client/src/Components/Home/Homepage.js
+++ client/src/Components/Home/Homepage.js
@@ -1,6 +1,73 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Homepage.scss'
+import { ethers } from 'ethers';
+import DeID from "../../artifacts/contracts/DeID.sol/DeID.json"
 const Homepage = () => {
+  const [connected, setconnected] = useState(false);
+  const [contract, setcontract] = useState(null);
+  const [provider, setprovider] = useState(null);
+  const [account, setaccount] = useState(null);
+  const connectFetch = async () => {
+    const loadProvider = async (provider) => {
+        if (provider) {
+            window.ethereum.on("chainChanged", () => {
+                loadProvider(provider);
+            });
+            window.ethereum.on("accountsChanged", () => {
+                loadProvider(provider);
+            });
+            const { ethereum } = window;
+            // try {
+            //     await ethereum.request({
+            //         method: 'wallet_switchEthereumChain',
+            //         params: [{ chainId: '0x13881' }],
+            //     });
+            // } catch (switchError) {
+            //     // This error code indicates that the chain has not been added to MetaMask.
+            //     if (switchError.code === 4902) {
+            //         // Do something
+            //         window.ethereum.request({
+            //             method: 'wallet_addEthereumChain',
+            //             params: [{
+            //                 chainId: '0x13881',
+            //                 chainName: 'Polygon',
+            //                 nativeCurrency: {
+            //                     name: 'Mumbai',
+            //                     symbol: 'MATIC',
+            //                     decimals: 18
+            //                 },
+            //                 rpcUrls: ['https://rpc-mumbai.maticvigil.com'],
+            //                 blockExplorerUrls: ['https://mumbai.polygonscan.com']
+            //             }]
+            //         })
+            //             .catch((error) => {
+            //             });
+            //     }
+            // }
+            await provider.send("eth_requestAccounts", []);
+            const signer = provider.getSigner();
+            const address = await signer.getAddress();
+            setaccount(address);
+            let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; //mumbai
+            const contractInstance = new ethers.Contract(
+                contractAddress,
+                DeID.abi,
+                signer
+            );
+            setcontract(contractInstance);
+            setprovider(provider);
+            setconnected(true);
+        } else {
+            console.error("MetaMask not Installed");
+        }
+    };
+    try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await loadProvider(provider);
+    } catch (error) {
+        console.error("MetaMask not Installed");
+    }
+}
   //Use effect to get the logged in details, to accordingly load user and company homepage
   return (
     <div>
@@ -9,12 +76,18 @@ const Homepage = () => {
           <h1 className='navbar__center--brand'>Group Project</h1>
         </div>
         <div className='navbar__right'>
+          {connected && 
           <button className='navbar__right--edit'>
-            Edit details
+            Edit Profile
           </button>
-          <button className='navbar__right--connect'>
+          }
+          {connected ? 
+          <button className='truncate max-w-[230px] navbar__right--connect'>Account : {account}</button>
+          :
+          <button onClick={connectFetch} className='navbar__right--connect'>
             Connect
           </button>
+        }
         </div>
       </div>
 
